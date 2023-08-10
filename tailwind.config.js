5c14a79967642e746fd3c40325d1d8e1a5d857a5
Pookinston <nicholashm987@gmail.com>
2023-08-10 14:33:43 +0700

    Third Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'AgeBot.html'],
   theme: {
     container: {
       center: true,
