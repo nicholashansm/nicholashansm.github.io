f7577eca32070f8f9c92feb2fd83642779cd01fa
Pookinston <nicholashm987@gmail.com>
2023-09-20 18:52:05 +0700

    20 Sept 2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'AgeBot.html'],
+  content: ['index.html', 'AgeBot.html', 'Research.html'],
   theme: {
     container: {
       center: true,
